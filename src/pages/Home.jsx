ec7c928ac18ab4a72322a077a7a3c6cee257c0f6
maryamessam22-pixel <maryamessam22@gmail.com>
2025-12-01 23:07:30 +0200

    Add Top Header At Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,25 @@ const Home = () => {
     return ( <>
 <div className="dashboard-layout">
 <Sidebar/>
+  <main className="main-content">
+  {/* Top Header */}
+        <header className="top-header">
+          <div className="breadcrumbs">Pages / <strong>Dashboard</strong></div>
+          <div className="header-actions">
+            <div className="search-bar">
+              <span className="search-icon">🔍</span>
+              <input type="text" placeholder="Search" />
+            </div>
+            <div className="notification-icon">🔔<div className="dot"></div></div>
+          </div>
+        </header>
 
 
 
 
 
 
-
-
-
-
-
-
+  </main>
 </div>
     
     
