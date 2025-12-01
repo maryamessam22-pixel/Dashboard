9455e82f51368a8397ffd1a682b9c1cc800e69a2
maryamessam22-pixel <maryamessam22@gmail.com>
2025-12-01 23:38:36 +0200

    Add Const Data About Traffic Chart From Library (recharts)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,45 @@ import './Home.css';
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 const Home = () => {
 
+    const data = [
+  {
+    name: 'A',
+    uv: 400,
+    pv: 240,
+    amt: 2400,
+  },
+  {
+    name: 'B',
+    uv: 300,
+    pv: 456,
+    amt: 2400,
+  },
+  {
+    name: 'C',
+    uv: 300,
+    pv: 139,
+    amt: 2400,
+  },
+  {
+    name: 'D',
+    uv: 200,
+    pv: 980,
+    amt: 2400,
+  },
+  {
+    name: 'E',
+    uv: 278,
+    pv: 390,
+    amt: 2400,
+  },
+  {
+    name: 'F',
+    uv: 189,
+    pv: 480,
+    amt: 2400,
+  },
+];
+
     return ( <>
 <div className="dashboard-layout">
 <Sidebar/>
