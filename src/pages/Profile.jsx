f9cb7cfc361c38a3a648ed5d6e90b972d9c67f1c
maryamessam22-pixel <maryamessam22@gmail.com>
2025-12-03 13:38:11 +0200

    Add Profile Screen

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Layout from '../layout/Layout';
+import Header from './../components/Header';
+const Profile = () => {
+    return ( <>
+    
+    
+    
+    <Header title="Pages/ Profile" />
+    <Layout/>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </> );
+}
+ 
+export default Profile;
