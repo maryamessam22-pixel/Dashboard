167df2d8d2db01481d4c60414873087eb105dbfe
maryamessam22-pixel <maryamessam22@gmail.com>
2025-12-17 21:45:47 +0200

    edit at SiteContentPage.css

--- src/pages/SiteContentPage.jsx
+++ src/pages/SiteContentPage.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from '../layout/Layout';
 import './SiteContentPage.css';
 import GeneralSettings from './../components/GeneralSettings';
 import HomePageSettings from './../components/HomePageSettings';
 import AboutPageSettings from './../components/AboutPageSettings';
 import Header from '../components/Header';
+import { supabase} from '../Supabase';
 
 
 const SiteContentPage = () => {
   const [activeTab, setActiveTab] = useState('General'); 
+     const [loading, setLoading] = useState(true);
+     const [sections, setSections] = useState("");
+
+
+useEffect(() => {
+
+  async function getAllSectionsAPI() {
+    const res = await supabase.from("page_sections").select("*");
+    setSections(res.data);
+    // console.log(res);
+    setLoading(false);
+  }
+
+  getAllSectionsAPI();
+
+}, []);
+
+if (loading) {
+  return (
+    <div className="loading-center">
+      <p>Loading...</p>
+    </div>
+  );
+}
+
 
   return (
     <>
