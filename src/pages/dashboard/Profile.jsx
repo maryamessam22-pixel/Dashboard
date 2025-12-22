c145ffe3dcaa4a79025f99e3ea2dd41ef7ae1903
maryamessam22-pixel <maryamessam22@gmail.com>
2025-12-22 03:31:47 +0200

    edit at profile.jsx

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import "./Profile.css";
 import Header from '../../layouts/Header';
-// Assuming Sectionnn was a generic section component, mapping to DashboardSection or similar if renamed. 
-// If specific file exists, path would be ../../components/dashboard/Sectionnn (or whatever new name)
-// For now, I'll assume it might be 'DashboardSection' based on your structure or kept as a component.
 import DashboardSection from "../../components/dashboard/DashboardSection"; 
 import Layout from '../../layouts/Layout';
 import ProfileCard from '../../components/common/ProfileCard';
@@ -25,7 +22,6 @@ const Profile = () => {
             <input className="input-field" type="text" placeholder="Full Name" />
             <input className="input-field" type="text" placeholder="Job Title" />
             <input className="input-field" type="email" placeholder="Email Address" />
-            {/* <button className="save-btn">Save Changes</button> */}
           </DashboardSection>
 
           <DashboardSection title="Contact Information">
